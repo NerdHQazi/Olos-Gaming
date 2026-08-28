2f1cbdf0df96752d6cdaae6c4ade8ccebc69c5c9
Benimo12-home <benonime2007@gmail.com>
2026-08-28 04:08:17 +0200

    Add files via upload

--- games/bounce/bounce_test.py
+++ games/bounce/bounce_test.py
@@ -1,11 +1,19 @@
 #Global libraries
 import sys
 import pygame
+from pygame.locals import *
+import os
 
 #Replace path with local path. These are for non-environmental libraries
-sys.path.insert(0,"C:/Users/../Olos-Gaming/games/")
-#sys.path.append
+#sys.path.insert(0,"C:/Users/benon/OneDrive/Desktop/Olos-Gaming/games/")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from lib import tilemap
 from lib.bounce_engine import Player, platform
-from lib.tilemap import tilemap
+from lib.tilemap.tilemap import Tileset, imageTileMap, TileMap
+
+#Make sure to import all classes to be used individually
+
+#Tile stuff
+tileset = Tileset("C:/Users/benon/OneDrive/Desktop/Olos-Gaming/games/assets/tmw_desert_spacing.webp") #Default tile size of 32*32 pixels and default margin and spacing of 1 pixel
+tilemap = imageTileMap(tileset, 30, 20) #Tilemap width of 30 and height of 20
