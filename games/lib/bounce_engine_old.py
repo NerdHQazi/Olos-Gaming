fcfbaf55e021f439be4ba5da50c7ba202508faf1
Benimo12-home <benonime2007@gmail.com>
2026-08-28 04:09:08 +0200

    Add files via upload

--- games/lib/bounce_engine_old.py
+++ games/lib/bounce_engine_old.py
@@ -1,59 +1,67 @@
 import pygame
 from pygame.locals import *
-import sys
-import random
-
+ 
 pygame.init()
-vec = pygame.math.Vector2 #2 for two dimensional
-
+vec = pygame.math.Vector2  # 2 for two dimensional
+ 
 HEIGHT = 450
 WIDTH = 400
 ACC = 0.5
 FRIC = -0.12
 FPS = 60
-
+ 
 FramePerSec = pygame.time.Clock()
-
+ 
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game")
 
+'''
 class Player(pygame.sprite.Sprite):
-    def __init__(self, file):
+    def __init__(self):
         super().__init__() 
-        #self.image = pygame.image.load(file)
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128,255,40))
+        self.rect = self.surf.get_rect(center = (10, 420))
+'''
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        #self.image=pygame.image.load()
         self.surf = pygame.Surface((30, 30))
         self.surf.fill((128,255,40))
         self.rect = self.surf.get_rect()
-  
-        self.pos = vec((10, 360))
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-
-    def move(self):
-        self.acc = vec(0,0.5)
    
+        self.pos = vec((10, 385))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0) #Change to ball physics for bounce
+    
+    def move(self):
+        self.acc = vec(0,0.5) #0.5 is the gravity
+ 
         pressed_keys = pygame.key.get_pressed()
-               
+            
         if pressed_keys[K_LEFT]:
             self.acc.x = -ACC
         if pressed_keys[K_RIGHT]:
             self.acc.x = ACC
-                
+        
         self.acc.x += self.vel.x * FRIC
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
         
+        '''
         if self.pos.x > WIDTH:
             self.pos.x = 0
         if self.pos.x < 0:
             self.pos.x = WIDTH
-            
-        self.rect.midbottom = self.pos
-
+        '''#This will wrap the player position around the screen
+        self.rect.midbottom = self.pos #This line should stay at the bottom
+        
     def jump(self):
         hits = pygame.sprite.spritecollide(self, platforms, False)
         if hits:
-           self.vel.y = -15
+            self.vel.y = -15
 
 
     def update(self):
@@ -63,17 +71,17 @@ class Player(pygame.sprite.Sprite):
                 self.vel.y = 0
                 self.pos.y = hits[0].rect.top + 1
 
-
 class platform(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.surf = pygame.Surface((WIDTH, 20))
         self.surf.fill((255,0,0))
         self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
-
+        
     def move(self):
         pass
-'''
+
+#'''
 PT1 = platform()
 P1 = Player()
 
@@ -83,26 +91,33 @@ all_sprites.add(P1)
 
 platforms = pygame.sprite.Group()
 platforms.add(PT1)
-'''
 
-while True: 
+
+
+while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    '''
         if event.type == pygame.KEYDOWN:    
             if event.key == pygame.K_SPACE:
                 P1.jump()
-    '''
-    '''    
+     
     displaysurface.fill((0,0,0))
-    P1.update()
-
+ 
     for entity in all_sprites:
         displaysurface.blit(entity.surf, entity.rect)
-        entity.move()
-    '''
-
+    P1.move()
     pygame.display.update()
     FramePerSec.tick(FPS)
+
+'''
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
+    FramePerSec.tick(FPS)
+'''
